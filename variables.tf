fbe40253064d86a6ad5c4b38ef9c688b2906b0e7
shivaji chate <shivaji.chate93@gmail.com>
2024-03-19 12:49:01 +0000

    test

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "hostname" {
+  type    = string
+  default = "test.com"
+}
+
+variable "location" {
+  type    = string
+  default = "North Central US"
+}
+
+variable "prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "tags" {
+  description = "Default tags to apply to all resources."
+  type        = map(any)
+}
+
